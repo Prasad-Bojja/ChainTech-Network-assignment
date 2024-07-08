a3afbb79b46af420d21cedfdc337462e66276a20
Prasad-Bojja <128887127+Prasad-Bojja@users.noreply.github.com>
2024-07-08 18:27:07 +0530

    Update forms.py
    updated bootstrap class

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,5 +2,5 @@
 from django import forms
 
 class SubmissionForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=100)
-    email = forms.EmailField(label='Email')
+    name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}))
