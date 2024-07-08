0ad1119234b77c04d1cd99ef7d10dfd6ef9c5f6b
Prasad-Bojja <128887127+Prasad-Bojja@users.noreply.github.com>
2024-07-08 18:23:59 +0530

    Update views.py
    index function updated

--- myapp/views.py
+++ myapp/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from datetime import datetime
 from .models import UserSubmission
+from .forms import SubmissionForm
 
 def index(request):
     if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        submission = UserSubmission(name=name, email=email)
-        submission.save()
+        form = SubmissionForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            submission = UserSubmission(name=name, email=email)
+            submission.save()
+            return redirect('/submissions/')
+
+    else:
+        form = SubmissionForm()
  
     context = {
         'current_time': datetime.now(),
+        'form': form
     }
     return render(request, 'index.html', context)
 
